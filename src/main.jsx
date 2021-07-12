757f57519e8db8ffc92954384a663ee1f2ef7d21
Tim Hatch <timhatch@MacBook-Pro.hsd1.il.comcast.net>
2021-07-12 18:11:01 -0500

    added milligram and updated dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import "milligram"
 
 //import router
 import {BrowserRouter as Router} from "react-router-dom"
